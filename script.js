ccf8ed834e5a2c21ba19072225e100e32d96148a
testprosto <163897213+testprosto@users.noreply.github.com>
2025-12-29 22:00:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
 const channels = [
-  {
-    name: "Animax (544p)",
-    // Используем CORS прокси для HTTPS
-    url: "https://corsproxy.io/?http://cdns.jp-primehome.com:8000/zhongying/live/playlist.m3u8?cid=bs15&checkedby:iptvcat.net",
-    logo: "https://ri.zzls.xyz/bYX8NqT.png"
-  }
+  {group:'Общие', name:'Animax', logo:'https://ri.zzls.xyz/bYX8NqT.png', url:'http://cdns.jp-primehome.com:8000/zhongying/live/playlist.m3u8?cid=bs15&checkedby:iptvcat.net'},
 ];
 
-const player = document.getElementById('iptvPlayer');
 
-if (Hls.isSupported()) {
-  const hls = new Hls();
-  hls.loadSource(channels[0].url);
-  hls.attachMedia(player);
-  hls.on(Hls.Events.MANIFEST_PARSED, () => {
-    player.play();
+const sidebar = document.getElementById('sidebar');
+const video = document.getElementById('video');
+const now = document.getElementById('now');
+let hls;
+
+const groups = {};
+
+// Сортируем каналы по группам
+channels.forEach(c => {
+  if(!groups[c.group]) groups[c.group] = [];
+  groups[c.group].push(c);
+});
+
+// Создаем интерфейс боковой панели
+for (const g in groups) {
+  const h = document.createElement('h3');
+  h.textContent = g;
+  sidebar.appendChild(h);
+
+  groups[g].forEach(ch => {
+    const div = document.createElement('div');
+    div.className='channel';
+    div.innerHTML = `<img src="${ch.logo}"><span>${ch.name}</span>`;
+    div.onclick = () => play(ch);
+    sidebar.appendChild(div);
   });
-} else if (player.canPlayType('application/vnd.apple.mpegurl')) {
-  player.src = channels[0].url;
-  player.addEventListener('loadedmetadata', () => {
-    player.play();
-  });
+}
+
+// Функция воспроизведения
+function play(ch) {
+  now.textContent = ch.name;
+  
+  if (hls) {
+    hls.destroy();
+    hls = null;
+  }
+
+  if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = ch.url;
+    video.play();
+  } else if (Hls.isSupported()) {
+    hls = new Hls();
+    hls.loadSource(ch.url);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+      video.play();
+    });
+  } else {
+    alert('HLS не поддерживается вашим браузером');
+  }
 }
