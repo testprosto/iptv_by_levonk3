b632fdfbf254458210dcbc096320969429b75fc2
testprosto <163897213+testprosto@users.noreply.github.com>
2025-12-29 22:34:36 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const channels = [
-  {group:'Общие', name:'Animax', logo:'https://ri.zzls.xyz/bYX8NqT.png', url:'http://cdns.jp-primehome.com:8000/zhongying/live/playlist.m3u8?cid=bs15&checkedby:iptvcat.net'},
-  {group:'Кино', name:'test2', logo:'http://epg.one/img/4663.png', url:'http://sewv654wfcsdwfi87fwvgbngh.siauliairsavlt.pw/iptv/8DN2FWQKBVS73T/31547/index.m3u8'},
-];
+function proxify(url) {
+  return `https://iptv-by-levonk3-git-main-levonk3s-projects.vercel.app/api/proxy?url=${encodeURIComponent(url)}`;
+}
 
+const channels = [
+  {group:'Общие', name:'Animax', logo:'https://ri.zzls.xyz/bYX8NqT.png', url: proxify('http://cdns.jp-primehome.com:8000/zhongying/live/playlist.m3u8?cid=bs15&checkedby:iptvcat.net')},
+  {group:'Кино', name:'test2', logo:'http://epg.one/img/4663.png', url: proxify('http://sewv654wfcsdwfi87fwvgbngh.siauliairsavlt.pw/iptv/8DN2FWQKBVS73T/31547/index.m3u8')},
+];
 
 const sidebar = document.getElementById('sidebar');
 const video = document.getElementById('video');
